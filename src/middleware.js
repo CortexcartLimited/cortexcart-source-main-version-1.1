bbd6d669fecfea00f6947f035e64155379ae09f9
CortexcartLimited <jonathanservice@hotmail.com>
2026-02-12 10:05:45 +0000

    feat: Add `script-src-elem` directive to the Content Security Policy header.

--- src/middleware.js
+++ src/middleware.js
@@ -24,6 +24,7 @@ const PATH_REQUIREMENTS = {
 const cspHeader = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.tiktok.com https://*.tiktokcdn.com https://sf-security.ibytedtos.com https://*.google.com https://*.googleapis.com https://*.gstatic.com https://*.stripe.com https://*.disqus.com https://*.disquscdn.com https://tracker.cortexcart.com;
+    script-src-elem 'self' 'unsafe-inline' https://*.tiktok.com https://*.tiktokcdn.com https://sf-security.ibytedtos.com https://*.google.com https://*.googleapis.com https://*.gstatic.com https://*.stripe.com https://*.disqus.com https://*.disquscdn.com https://tracker.cortexcart.com;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://tracker.cortexcart.com;
     img-src 'self' blob: data: https:;
     font-src 'self' https://fonts.gstatic.com https://tracker.cortexcart.com;
